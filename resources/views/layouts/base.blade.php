3c448a8112b1a8fe7e2d12d392852b271eed7839
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2024-07-15 20:08:56 +0330

    updated livewire config and layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -3,23 +3,19 @@
     <head>
         <meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Dynamic meta and title -->
 		@isset($metaKeys) <meta name="keywords" content="{{$metaKeys}}"> @endisset
 		@isset($metaAuthor) <meta name="author" content="{{$metaAuthor}}"> @endisset
 		@isset($metaDescription) <meta name="description" content="{{$metaDescription}}"> @endisset
-		<title>@hasSection('title') @yield('title') - @endif{{ config('app.name') }}</title>
-
+		<title>@isset($title) {{$title . ' - '}} @endisset {{ config('app.name') }}</title>
         <!-- Favicon -->
 		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
-
         <!-- Fonts -->
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-        @livewireStyles
-		<!-- Livewire script config -->
-		@livewireScriptConfig
-		<!-- End of Livewire script config -->
-		@stack('scripts')
+		<!-- Build styles and scripts -->
+		@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+		@livewireStyles
+		<!-- stack styles -->
 		@stack('styles')
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -27,5 +23,9 @@
 
     <body>
         @yield('body')
+		<!-- livewire scripts -->
+		@livewireScriptConfig
+		<!-- stack scripts -->
+		@stack('scripts')
     </body>
 </html>
