fc1fedecd6fbc86498da05fb9504ed575adb5051
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2024-07-16 10:28:21 +0330

    added logout button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,18 @@
             <div class="p-6 text-right sm:fixed sm:top-0 sm:right-0">
                 @auth
                     <a href="{{ route('home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Home</a>
-                @else
+					<form
+						action="{{ route('logout') }}"
+						method="POST"
+						class="inline-block ml-4"
+					>
+						@csrf
+						<button
+							type="submit"
+							class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500"
+						>Logout</button>
+					</form>
+				@else
                     <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-indigo-500">Log in</a>
 
                     @if (Route::has('register'))
