4233753c8567a2b032277fd752eff1d61652cb19
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2024-08-13 19:22:18 +0330

    added a command to reset daily habits

---
+++ app/Console/Commands/ResetDailyHabits.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use \App\Models\Task;
+
+class ResetDailyHabits extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:reset-daily-habits';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Resets the is_done field to 0 for daily habits.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+		// Update the tasks table
+		Task::where('is_daily_habit', 1)->update(['is_done' => 0]);
+
+		$this->info('Daily habits reset successfully!');
+    }
+}
