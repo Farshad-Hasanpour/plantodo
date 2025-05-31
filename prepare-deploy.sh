04144b10d05ecacc66ca4742590d409f451065e2
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2025-05-31 15:48:13 +0330

    include vendor in the workflow only when composer files have changed

--- prepare-deploy.sh
+++ prepare-deploy.sh
@@ -2,16 +2,18 @@
 # Define Upload Directories
 PUBLIC_DIR="upload/public"
 PRIVATE_DIR="upload/private"
+VENDOR_DIR="upload/vendor"
 
 # Create Upload Directories
 mkdir -p "$PUBLIC_DIR"
 mkdir -p "$PRIVATE_DIR"
+mkdir -p "$VENDOR_DIR"
 
 # Copy public directory
 cp -r public "$PUBLIC_DIR"
 
 # Copy private directories
-for dir in app bootstrap config resources routes storage vendor; do
+for dir in app bootstrap config resources routes storage; do
   cp -r "$dir" "$PRIVATE_DIR/$dir"
 done
 
@@ -20,3 +22,7 @@ mkdir -p "$PRIVATE_DIR/public/build"
 cp public/build/manifest.json "$PRIVATE_DIR/public/build/manifest.json"
 cp .env "$PRIVATE_DIR/.env"
 cp artisan "$PRIVATE_DIR/artisan"
+
+# Separate vendor files because it is uploaded only when composer file is changed
+cp -r vendor "$VENDOR_DIR"
+
